9528b20f1598f2f2668bb5c65c14585c460f19c7
Robert Lowe <robertmlowe@rmlowe.com>
2019-10-26 17:46:21 +0800

    Working With Directories

--- 3/3-2/readDirectoryDemo.js
+++ 3/3-2/readDirectoryDemo.js
@@ -1,9 +1,9 @@
 var fs = require('fs');
 
 //Mac users
-// fs.readdir('/', function(err,data){
+fs.readdir('/', function(err, data) {
+  console.log(data);
+});
+// fs.readdir('c:/', function(err,data){
 //     console.log(data);
 // });
-fs.readdir('c:/', function(err,data){
-    console.log(data);
-});
